d3a1604ebdbcfe6c8b76da459cfb62c8365afb09
RommelOlachea <msc.rommel.olachea@gmail.com>
2022-03-03 20:59:47 -0700

    actualizacion al js de la carpeta es9

--- src/es9/index.js
+++ src/es9/index.js
@@ -1,37 +1,44 @@
+//OPERADOR DE REPOSO (CECC)
 const obj = {
-    name: 'Rommel',
-    age: 40,
-    country: 'MX'
+    name: 'rommel',
+    age:40,
+    country:'MX'
 };
 
 let {country, ...all} = obj;
 console.log(country);
-console.log(obj);
+console.log(all);
 
+//PROPIEDADES DE PROPAGACION PARA UNIR UNO O MAS OBJETOS.
 const obj = {
-    name : 'Rommel',
-    age : 40  
+    name: 'rommel',
+    age:40,
 };
 
-const ob1 = {
-        ...obj,
-        country : 'MX'
-}
+const obj1 = {
+    ...obj,
+    country:'MX'
+};
 
-console.log(ob1);
+console.log(obj1);
 
-//PROMISE AND FINALLY
-const helloWorld = () => {
-    return new Promise((resolve,reject) => {
+//FINALLY EN LAS PROMESAS 
+
+const helloWord = () => {
+    return new Promise((resolve,reject)=>{
         (true)
-            ? setTimeout(() => resolve('Hello world!!'), 3000)
-            : reject(new Error('Test error'));
+            ? setTimeout(() => resolve('Hello World!!'), 3000)
+            : reject(new Error('Test Error'));
     });
 };
 
-helloWorld()
-    .then(response =>console.log(response))
+helloWord()
+    .then(response => console.log(response))
     .catch(error => console.log(error))
-    .finally(()=>console.log('Finally'));
+    .finally(()=>console.log('Finalizo la promesa'));
 
+//AGRUPACION DE REGEX
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match = regexData.exec('2022-03-03');
 
+console.log(match[1], match[2], match[3]);
