17b386e2a42685670902315a243311b6de33edcf
RommelOlachea <msc.rommel.olachea@gmail.com>
2022-03-08 14:58:48 -0700

    Modificacion de test sin alteracion de codigo

--- src/es12/index.js
+++ src/es12/index.js
@@ -14,13 +14,47 @@ class message{
         console.log(val);
     };
     get #add(value){
-        ...
+        ...logica definida
     }
     set #set(value){
-        ...
+        ...logica definida
     }
 }
 
 const msg = new message();
 msg.show('Hola');
 msg.show2('Hola');
+
+//promise any
+const promise1 = new Promise((resolve,reject)=> reject('1'));
+const promise2 = new Promise((resolve,reject)=> resolve('2'));
+const promise3 = new Promise((resolve,reject)=> resolve('3'));
+
+Promise.any([promise1,promise2,promise3])
+    .then(response => console.log(response));
+
+//weak reference
+class anyClass {
+    constructor(elemento){
+        this.ref  = new WeakRef(elemento);
+    }
+}
+
+//Asignación and lógico, asignación or lógico, asignación de anulación lógica
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue &&= isFalse);
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue ||= isFalse);
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue ??= isFalse);
+
+//Nullish coalescing operator (??)
+const fooo = null ?? 'default string';
+console.log(fooo);
+
+
