3e59eda1936f623a5d890b3615cbdf7be3048c0a
RommelOlachea <msc.rommel.olachea@gmail.com>
2022-03-08 09:29:24 -0700

    ecmascript 2021 - cecc

--- src/es12/index.js
+++ src/es12/index.js
@@ -1,1 +1,26 @@
-const str = "JavaScript es maravilloso "
+//replace y replaceAll
+const str = "JavaScript es maravillos, con JavaScript puedo crear el futuro de la web";
+const replaced = str.replace('JavaScript','Phyton');
+const replaced2 = str.replaceAll('JavaScript','Phyton');
+console.log(replaced);
+console.log(replaced2);
+
+//metodos privados en las clases
+class message{
+    show(val){
+        console.log(val);
+    };
+    #show2(val){
+        console.log(val);
+    };
+    get #add(value){
+        ...
+    }
+    set #set(value){
+        ...
+    }
+}
+
+const msg = new message();
+msg.show('Hola');
+msg.show2('Hola');
